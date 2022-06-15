5bd2cb1b7e31f65709029aec997fc139de5f6a43
weklihua <wek.lihua@gmail.com>
2022-06-14 21:29:55 -0700

    finish part 1

--- models/skill.js
+++ models/skill.js
@@ -1,16 +1,21 @@
 const skills = [
-    {id: 001, skill: 'Javascript', done: true},
-    {id: 002, skill: 'Python', done: false},
-    {id: 003, skill: 'REACT', done: false}, 
-    {id: 004, skill: 'html', done: true},
-    {id: 005, skill: 'css', done: true},
+    {id: 1, skill: 'Javascript', done: true},
+    {id: 2, skill: 'Python', done: false},
+    {id: 3, skill: 'REACT', done: false}, 
+    {id: 4, skill: 'html', done: true},
+    {id: 5, skill: 'css', done: true},
   ];
   
   module.exports = {
-    getAll
+    getAll,
+    getOne,
   };
   
   function getAll() {
     return skills;
   }
  
+  function getOne(id) {
+    id = parseInt(id)
+    return skills.find(skill => skill.id === id)
+  }
--- routes/skills.js
+++ routes/skills.js
@@ -5,5 +5,6 @@ var router = express.Router();
 
 const skillsCtrl = require('../controllers/skills')
 router.get('/', skillsCtrl.index)
+router.get('/:id', skillsCtrl.show)
 
 module.exports = router;
