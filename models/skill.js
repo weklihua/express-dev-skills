38b70c539d71b4e453bf112440cfec714deb6f57
weklihua <wek.lihua@gmail.com>
2022-06-14 19:24:29 -0700

    add more skills

--- models/skill.js
+++ models/skill.js
@@ -1,7 +1,9 @@
 const skills = [
-    {id: 125223, skill: 'Javascript', done: true},
-    {id: 127904, skill: 'Python', done: false},
-    {id: 139608, skill: 'REACT', done: false}
+    {id: 001, skill: 'Javascript', done: true},
+    {id: 002, skill: 'Python', done: false},
+    {id: 003, skill: 'REACT', done: false}, 
+    {id: 004, skill: 'html', done: true},
+    {id: 005, skill: 'css', done: true},
   ];
   
   module.exports = {
